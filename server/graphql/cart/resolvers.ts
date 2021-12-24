57ab33da9bc9d97ff6027e1cd7d5288e1df4d1d0
christianquispe <christianquispecamasca@gmail.com>
2021-12-24 14:13:20 -0500

    chore: implement all cart resolver

--- server/graphql/cart/resolvers.ts
+++ server/graphql/cart/resolvers.ts
@@ -43,6 +43,57 @@ const resolvers = {
       logger.info(ctx, "Finished!");
       return response;
     },
+    allCart: async (
+      _root: any,
+      args: { paginated: IPaginate; filter: IFilter },
+      _context: any
+    ) => {
+      const ctx = "GET_PICTURES";
+      logger.info(ctx, "Starting...");
+      // Input
+      const {
+        filter = { q: undefined, ids: undefined, status: undefined },
+        paginated = { page: 1, perPage: 10 },
+      } = args;
+
+      const { page = 1, perPage = 10 } = paginated;
+      const { q, ids } = filter;
+
+      const aggs: TMatchAggs = { $match: {} };
+
+      if (q) {
+        const reg = { $regex: new RegExp(escapeRegExp(q.trim()), "i") };
+        aggs.$match["$or"] = [
+          { "productSummary.name": reg },
+          { "productSummary.description": reg },
+        ];
+      }
+
+      if (ids) {
+        ids.forEach((id) => new moongose.Types.ObjectId(id));
+        aggs.$match["_id"] = { $in: ids };
+      }
+
+      logger.info(ctx, "Working...");
+      const pageInfo = await CartService.getPaginated({
+        page,
+        perPage,
+        aggs: aggs.$match,
+      });
+
+      const cart = await CartService.getAll(aggs.$match);
+      logger.info(ctx, "Validating if exist...");
+
+      // Data response
+      const response = {
+        pageInfo,
+        cart: cart,
+      };
+
+      // Response
+      logger.info(ctx, "Finished!");
+      return response;
+    },
   },
   Mutation: {
     addToCart: async (_root: any, args: IArgs<IAddToCart>, _context: any) => {
